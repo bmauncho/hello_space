1afbf2aa84345c0ddebd449b19f154e3e62815ad
BradleyMauncho <bradley@mekangames.com>
2024-10-26 15:44:01 +0300

    update

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnc5b9po-z0NvEkgU9YampOkAKAnazrDk",
+  authDomain: "hellospacedemo.firebaseapp.com",
+  projectId: "hellospacedemo",
+  storageBucket: "hellospacedemo.appspot.com",
+  messagingSenderId: "55580719035",
+  appId: "1:55580719035:web:8fbb61909a77a887e8f69e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
